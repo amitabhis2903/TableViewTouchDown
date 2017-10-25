6705c34874ccb5e538f41d4ad0e25f513835e060
Ammy <ammypandey@Ammys-MacBook-Air.local>
2017-10-25 11:53:01 +0530

    Initial Commit

---
+++ TableViewTouchDown/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewTouchDown
+//
+//  Created by Ammy Pandey on 25/10/17.
+//  Copyright © 2017 Ammy Pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
